5dc45b32ef6cf077546fe7b40a06fddcdaa8f50f
IceLitty <LitAlways@gmail.com>
2018-11-07 01:14:07 +0800

    Use world.spawnParticle to replace old effect function. Support block meta value.

--- src/com/nisovin/magicspells/spelleffects/BlockBreakEffect.java
+++ src/com/nisovin/magicspells/spelleffects/BlockBreakEffect.java
@@ -1,8 +1,9 @@
 package com.nisovin.magicspells.spelleffects;
 
-import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.Particle;
+import org.bukkit.material.MaterialData;
 
 import com.nisovin.magicspells.DebugHandler;
 
@@ -36,6 +37,7 @@ import com.nisovin.magicspells.DebugHandler;
 public class BlockBreakEffect extends SpellEffect {
 
 	int id = 1;
+	int meta = 0;
 	
 	@Override
 	public void loadFromString(String string) {
@@ -51,11 +53,12 @@ public class BlockBreakEffect extends SpellEffect {
 	@Override
 	public void loadFromConfig(ConfigurationSection config) {
 		id = config.getInt("id", id);
+        meta = config.getInt("meta", meta);
 	}
 	
 	@Override
 	public Runnable playEffectLocation(Location location) {
-		location.getWorld().playEffect(location, Effect.STEP_SOUND, id);
+        location.getWorld().spawnParticle(Particle.BLOCK_CRACK, location.add(0, 0.5, 0), 100, new MaterialData(id, (byte)meta));
 		return null;
 	}
 	
